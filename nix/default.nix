05ba0e4d921630e4764576e1aef98d045d9c3d2a
Christos Charisis <christos@scio.systems>
2022-11-14 11:22:55 +0200

    initial commit

---
+++ nix/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, python, python3Packages }:
+
+let
+  self = rec {
+    callPackage = lib.callPackageWith (pkgs // python3Packages // self);
+
+    # The folowing comment line is use to inject packages, do not change it.
+    # ADD NEW PACKAGE HERE
+    # executing = callPackage ./executing.nix { };
+    varname = callPackage ./varname.nix { };
+    diot = callPackage ./diot.nix { };
+    pipda = callPackage ./pipda.nix { };
+    datar = callPackage ./datar.nix { };
+  };
+in self
